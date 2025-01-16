b0583636d0b1a45964c5f3c5503bbe515d9f9009
Sameed <sameedhassan22@gmail.com>
2025-01-16 16:46:15 +0500

    Change expired purchases removal command schedule from every minute to daily

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::command('purchases:remove-expired')->everyMinute()->onOneServer();
+Schedule::command('purchases:remove-expired')->daily()->onOneServer();
