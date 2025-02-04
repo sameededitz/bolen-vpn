ec2a5825630a50fe67c233ef22a0a3720c99de66
Sameed <sameedhassan22@gmail.com>
2025-02-04 16:41:14 +0500

    Update sliders table to change description column to longText with binary charset

--- database/migrations/2024_12_27_154903_create_sliders_table.php
+++ database/migrations/2024_12_27_154903_create_sliders_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('sliders', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('description');
+            $table->longText('description')->charset('binary');
             $table->timestamps();
         });
     }
